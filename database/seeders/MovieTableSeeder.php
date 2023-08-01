a6fd3cd669932e37550cf4706c9a9b9d6a881c19
Tri Kurniawan <trikurniawan@it.kotamobagu.go.id>
2023-08-01 15:13:04 +0800

    feat: migration and seeders files

---
+++ database/seeders/MovieTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Movie;
+
+class MovieTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $movies = [
+            [
+                'name' => 'The Shawshank Redemption',
+                'slug' => 'the-shamshank-redention',
+                'category' => 'Drama',
+                'video_url' => 'https://www.youtube.com/watch?v=0zIJbutQuts',
+                'thumbnail' => 'https://m.media-amazon.com/images/I/512G9J05RJL._AC_UF1000,1000_QL80_.jpg',
+                'rating' => 9.3,
+                'is_featured' => 1,
+            ],
+
+            [
+                'name' => 'The GodFather',
+                'slug' => 'the-god-father',
+                'category' => 'Action',
+                'video_url' => 'https://www.youtube.com/watch?v=0zIJbutQuts',
+                'thumbnail' => 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
+                'rating' => 9.1,
+                'is_featured' => 0,
+            ],
+            [
+                'name' => 'The GodFather: Part II',
+                'slug' => 'the-god-father',
+                'category' => 'Action',
+                'video_url' => 'https://www.youtube.com/watch?v=0zIJbutQuts',
+                'thumbnail' => 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
+                'rating' => 9.0,
+                'is_featured' => 0,
+            ]
+
+        ];
+
+        Movie::insert($movies);
+    }
+}
